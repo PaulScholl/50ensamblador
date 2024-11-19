fda0df09c4779af0efaf3721c359984d61c32358
Alan Paul Rodriguez Gallardo <147470070+PaulScholl@users.noreply.github.com>
2024-11-18 17:46:08 -0800

    Update 6.s

--- 6.s
+++ 6.s
@@ -1,8 +1,6 @@
 // Archivo: 6.s
 // Autor: Rodriguez Gallardo Alan Paul
-// Objetivo: Convertir grados celsius a Farenheit 
-
-//// Suma de los N primeros números naturales
+// Objetivo: Suma de los N primeros números naturales
 //// Registros utilizados:
 //// X0 - Parámetro N y resultado final
 //// X1 - Contador actual
